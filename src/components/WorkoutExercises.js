4755760256a495541508eeee2a1ba2f6655711aa
JohnKazantzis <gkazantzis96@gmail.com>
2025-04-07 08:41:34 +0300

    Fixed workout exercise-set UI

--- src/components/WorkoutExercises.js
+++ src/components/WorkoutExercises.js
@@ -23,11 +23,24 @@ export default function WorkoutExercises({ workoutId }) {
         <div className="workout-exercises-box"> 
             {workoutExercises.map(exercise => {
                 return (
-                    <div>
-                        <div>{exercise.exerciseName}</div>
+                    <div className="exercise-sets" key={exercise.id}>
+                        <div className="mb-1">{exercise.exerciseName}</div>
                         {exercise.workoutSets.map(set => {
                             return (
-                                <div>{`Set: ${set.setNumber}  weight: ${set.weight}  reps: ${set.reps}`}</div>
+                                <div key={set.setNumber} className="is-flex is-justify-content-space-between set ml-5">
+                                    <figure className="image is-24x24 is-flex">
+                                        <img src="/icons8-hashtag-32.png" alt="#"/>
+                                        {set.setNumber}
+                                    </figure>
+                                    <figure className="image is-24x24 is-flex">
+                                        <img src="/icons8-weight-kg-32.png" alt="KG"/>
+                                        {set.weight}
+                                    </figure>
+                                    <figure className="image is-24x24 is-flex">
+                                        <img src="/icons8-repeat-32.png" alt="Reps"/>
+                                        {set.reps}
+                                    </figure>
+                                </div>
                             )
                         })}
                     </div>
